d444421d7534257ff3a6219a2990f164019f1446
future <mirech93@naver.com>
2023-11-11 23:13:34 +0900

    project commit

---
+++ src/Input.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+function Input() {
+    const [inputs, setInputs] = useState({
+        subject : "",
+        score: "",
+
+    });
+
+    const onChange = ( event ) => {
+        const { value, name } = event.target;    // 이벤트가 일어나고 있는 타겟 DOM을 찾는것
+        setInputs({
+            ...inputs,
+            [name] : value // dynamic property assign
+        })
+    }
+
+    return (<>
+        <div>
+            <h2>성적 : {inputs.subject}{inputs.score} </h2>
+            <input name="subject" placeholder="수학" value={inputs.subject} />
+            <input name="score" placeholder="99" value={inputs.score} />
+        </div>
+    </>);
+}
+
+export default Input;
