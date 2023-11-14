4ac8712965083559dbf91bff5ae1a4fb9e35e248
future <mirech93@naver.com>
2023-11-14 21:36:31 +0900

    2023-11-13 13강

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from "react";
-import Input from "./Input";
+import React, {useState, useEffect } from "react";
 
 function App(){
-  return  <Input/>
-}
+  const [ count, setCount ] = useState(0);
 
+  useEffect(
+    () => {
+      console.log("useEffect");
+      document.title = 'You clicked ${count} times';
+    }
+  )
+
+    return (
+      <>
+        <p> You clicked {count} times </p>
+        <button onClick={
+          () => {
+            console.log("Click");
+            setCount(count + 1);
+          }
+        }>Clieck Me</button>
+      </>
+    )
+}
 export default App;
