62c051cabe29ed318b44a13bfaa319d0a411b378
future <mirech93@naver.com>
2023-11-14 22:04:04 +0900

    2023-11-14 빡공단 40기 14강. 리액트에게 주소를 알려주는 Hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React, {useState, useEffect } from "react";
+import React from "react"; // import useState
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useNavigate
+} from "react-router-dom";
 
-function App(){
-  const [ count, setCount ] = useState(0);
+function App() {
+  return (
+    <BrowserRouter>
+      <button>
+        <Link to="/home">Home</Link>
+      </button>
+      <button>
+        <Link to="/">Index</Link>
+      </button>
 
-  useEffect(
-    () => {
-      console.log("useEffect");
-      document.title = 'You clicked ${count} times';
-    }
-  )
-
-    return (
-      <>
-        <p> You clicked {count} times </p>
-        <button onClick={
-          () => {
-            console.log("Click");
-            setCount(count + 1);
-          }
-        }>Clieck Me</button>
-      </>
-    )
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+function Index() {
+  var navigate = useNavigate();
+  function handleClick() {
+    navigate("/home");  // 입력받은 경로로 이동한다
+  }
+
+  return (
+    <>
+      <h1>Index</h1>
+      <button onClick={handleClick}>Go home</button>
+    </>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <h2>Home</h2>
+      <p>This is home</p>
+    </>
+  );
+}
+
 export default App;
