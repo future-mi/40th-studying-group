583f84f5a991f8c10ce30da0c86c547e1ab2aa0e
future <mirech93@naver.com>
2023-11-16 23:32:01 +0900

    2023-11-16 빡공단 40기 15강.나만의 Hook 만들기

--- src/App.js
+++ src/App.js
@@ -1,51 +1,51 @@
-import React from "react"; // import useState
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-  useNavigate
-} from "react-router-dom";
+import { useState } from "react"; // import useState
 
 function App() {
-  return (
-    <BrowserRouter>
-      <button>
-        <Link to="/home">Home</Link>
-      </button>
-      <button>
-        <Link to="/">Index</Link>
-      </button>
+  // const [click1, setClick1] = useState(false);
+  // const [click2, setClick2] = useState(false);
+  // const [click3, setClick3] = useState(false);
+  // const [click4, setClick4] = useState(false);
+  // const [click5, setClick5] = useState(false);
 
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+  // const toggle1 = () => {
+  //   setClick1((click1) => !click1);
+  // };
+  // const toggle2 = () => {
+  //   setClick2((click2) => !click2);
+  // };
+  // const toggle3 = () => {
+  //   setClick3((click3) => !click3);
+  // };
+  // const toggle4 = () => {
+  //   setClick4((click4) => !click4);
+  // };
+  // const toggle5 = () => {
+  //   setClick5((click5) => !click5);
+  // };
 
-function Index() {
-  var navigate = useNavigate();
-  function handleClick() {
-    navigate("/home");  // 입력받은 경로로 이동한다
-  }
+  const [click1, toggle1] = useToggle(false);
+  const [click2, toggle2] = useToggle(false);
+  const [click3, toggle3] = useToggle(false);
+  const [click4, toggle4] = useToggle(false);
+  const [click5, toggle5] = useToggle(false);
 
   return (
     <>
-      <h1>Index</h1>
-      <button onClick={handleClick}>Go home</button>
+      <button onClick={toggle1}> {click1 ? "Hello" : "Goodbye"} </button>
+      <button onClick={toggle2}> {click2 ? "Hello" : "Goodbye"} </button>
+      <button onClick={toggle3}> {click3 ? "Hello" : "Goodbye"} </button>
+      <button onClick={toggle4}> {click4 ? "Hello" : "Goodbye"} </button>
+      <button onClick={toggle5}> {click5 ? "Hello" : "Goodbye"} </button>
     </>
   );
 }
 
-function Home() {
-  return (
-    <>
-      <h2>Home</h2>
-      <p>This is home</p>
-    </>
-  );
+const useToggle = (defaultValue = false) => {
+  const [click, setClick ] = useState( defaultValue )
+  const toggle = () => setClick((click)=>!click)
+
+  return [click, toggle];
 }
+
 
 export default App;
