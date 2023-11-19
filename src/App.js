72465a4b459233d07bb3bc9ecd7b6847614f0f7a
future <mirech93@naver.com>
2023-11-19 21:05:31 +0900

    2023-11-19 빡공단 40기 19강. 첫 화면, 메인 페이지 만들기

--- src/App.js
+++ src/App.js
@@ -1,50 +1,24 @@
-import { useState } from "react"; // import useState
+import React from "react";
+import Items from "./components/Items";
+import Header from "./components/Header";
+import CreateItem from "./components/CreateItem";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  // const [click1, setClick1] = useState(false);
-  // const [click2, setClick2] = useState(false);
-  // const [click3, setClick3] = useState(false);
-  // const [click4, setClick4] = useState(false);
-  // const [click5, setClick5] = useState(false);
-
-  // const toggle1 = () => {
-  //   setClick1((click1) => !click1);
-  // };
-  // const toggle2 = () => {
-  //   setClick2((click2) => !click2);
-  // };
-  // const toggle3 = () => {
-  //   setClick3((click3) => !click3);
-  // };
-  // const toggle4 = () => {
-  //   setClick4((click4) => !click4);
-  // };
-  // const toggle5 = () => {
-  //   setClick5((click5) => !click5);
-  // };
-
-  const [click1, toggle1] = useToggle(false);
-  const [click2, toggle2] = useToggle(false);
-  const [click3, toggle3] = useToggle(false);
-  const [click4, toggle4] = useToggle(false);
-  const [click5, toggle5] = useToggle(false);
 
   return (
-    <>
-      <button onClick={toggle1}> {click1 ? "Hello" : "Goodbye"} </button>
-      <button onClick={toggle2}> {click2 ? "Hello" : "Goodbye"} </button>
-      <button onClick={toggle3}> {click3 ? "Hello" : "Goodbye"} </button>
-      <button onClick={toggle4}> {click4 ? "Hello" : "Goodbye"} </button>
-      <button onClick={toggle5}> {click5 ? "Hello" : "Goodbye"} </button>
-    </>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Items/>} />
+          <Route path="/todo" element={<Items/>} />
+          <Route path="/done" element={<Items/>} />
+          <Route path="/create_item" element={<CreateItem />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
-}
-
-const useToggle = (defaultValue = false) => {
-  const [click, setClick ] = useState( defaultValue )
-  const toggle = () => setClick((click)=>!click)
-
-  return [click, toggle];
 }
 
 
---
+++ src/components/Items.js
@@ -0,0 +1,26 @@
+export default function Items(){
+    return <table>
+        <td></td>
+        <td>Task</td>
+        <td>Due</td>
+        <td></td>
+        <tr>
+            <td></td>
+            <td>커피마시기</td>
+            <td>20231130</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>커피마시기</td>
+            <td>20231130</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>커피마시기</td>
+            <td>20231130</td>
+            <td></td>
+        </tr>
+    </table>
+}
