b502714cf915a48950e847291f4b7376c1b8a5c6
future <mirech93@naver.com>
2023-11-23 22:00:36 +0900

    2023-11-23 빡공단 40기 23강. 데이터를 추가하는 기능을 만들어봅니다

--- src/components/CreateItem.js
+++ src/components/CreateItem.js
@@ -1,3 +1,53 @@
+import {useRef, useState, useEffect} from "react";
+import {useNavigate} from "react-router-dom";
+
 export default function CreateItem(){
-    return <div></div>
+    const [values, setValues] = useState({
+        task:"", due:""
+    })
+
+    const handleChange = (event) =>{
+        const{name,value} = event.target;
+        setValues({
+            ...values,
+            [name]:value,
+        });
+    };
+
+    const navigate = useNavigate();
+    const ref = useRef();
+
+    useEffect(()=>{
+        ref.current.focus();   // 이곳으로 포커스가맞춰짐
+    })
+
+    const onSubmit = (event) => {
+        event.preventDefault(); // 화면이 새로고침 되는걸 막아줌
+        fetch("http://localhost:3001/items",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({
+                task: values.task,
+                due: values.due,
+                status: "todo",
+            }), 
+        }).then((res)=>{
+            if(res.ok){
+                alert("Created new item!");
+                navigate("/todo");
+            }
+        });
+    };
+
+    return <form onSubmit={onSubmit}>
+        <div className="input_area">
+            <p>Task</p>
+            <input type="text" name="task" value={values.task} onChange={handleChange} ref={ref}/>     
+            <p>Due</p>
+            <input type="text" name="due" value={values.due} onChange={handleChange}/>     
+        </div>
+        <button>Create</button>
+     </form>
 }
