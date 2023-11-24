9e8aa6382e68b4b750d13e7350bbb461f73666de
future <mirech93@naver.com>
2023-11-24 22:10:19 +0900

    2023-11-24 빡공단 40기 24강. 데이터를 삭제하는 기능을 만들어봅니다

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,14 +2,52 @@ import { useState } from "react";
 
 export default function Items({ item: i }) {
   const [item, setItem] = useState(i);
+
+  const toggle = () => {
+    fetch(`http://localhost:3001/items/${item.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...item,
+        status: item.status === "done" ? "todo": "done",
+      })
+    }).then((res) =>{
+      if(res.ok) {
+        setItem({
+          ...item,
+          status: item.status === "done" ? "todo": "done",
+        })
+      }
+    })
+  }
+
+  const deleteItem = () =>{
+    if(window.confirm("삭제하시겠습니까?")) {
+      fetch(`http://localhost:3001/items/${item.id}`, {
+        method: "DELETE",
+      }).then((res) => {
+        if(res.ok) {
+          setItem({ id: 0 });
+        }
+      });
+    }
+  };
+
+  if(item.id === 0) {
+    return null;
+  }
+
+
   return (
     <tr>
       <td>
-        <input type="checkbox" checked={item.status === "done" ? true : false}/>
+        <input type="checkbox" checked={item.status === "done" ? true : false} onClick={toggle}/>
       </td>
       <td>{item.task}</td>
       <td>{item.due}</td>
-      <td></td>
+      <td><button onClick={deleteItem}>Delete</button></td>
     </tr>
   );
 }
